d0fd691a5824733d90e36ee9cfcc8289b48dcb4c
GyayakP <gyayakpatodi@gmail.com>
2026-07-06 18:51:11 +0530

    Create 31-NextPermutation

---
+++ 31-NextPermutation/31-NextPermutation.cpp
@@ -0,0 +1,37 @@
+// Last updated: 7/6/2026, 6:51:11 PM
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+        // int n=nums.size();
+        // int pivot = -1;
+
+        // for(int i=n-2;i>=0;i--)
+        // {
+        //     if(nums[i]<nums[i+1])
+        //     {
+        //         pivot=i;
+        //         break;
+        //     }
+        // }
+
+        // if(pivot==-1)
+        // {
+        //     reverse(nums.begin(), nums.end());
+        //     return;
+        // }
+
+        // for(int i=n-1; i>pivot;i--)
+        // {
+        //     if(nums[i]>nums[pivot])
+        //     {
+        //         swap(nums[i], nums[pivot]);
+        //         break;
+        //     }
+        // }
+
+        // reverse(nums.begin()+pivot +1, nums.end());
+        // return;
+
+    }
+};
