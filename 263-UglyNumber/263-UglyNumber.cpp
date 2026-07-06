a0e3b97f496c9894bc156bcd58d176be4a7062dc
GyayakP <gyayakpatodi@gmail.com>
2026-07-06 18:49:57 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/6/2026, 6:49:57 PM
+class Solution {
+public:
+    bool isUgly(int n) 
+    {
+        if(n<=0)
+        {
+            return false;
+        }
+        while(n%2 ==0) 
+            n/=2;
+        while(n%3 ==0) 
+            n/=3;
+        while(n%5 ==0)
+            n/=5;
+        return n==1;
+    }
+};
