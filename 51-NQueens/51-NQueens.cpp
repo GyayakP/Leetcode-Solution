b4420a53e2b4d21f86a6b7b3ee46a1afbf033a89
GyayakP <gyayakpatodi@gmail.com>
2026-07-06 18:50:55 +0530

    Create 51-NQueens

---
+++ 51-NQueens/51-NQueens.cpp
@@ -0,0 +1,59 @@
+// Last updated: 7/6/2026, 6:50:55 PM
+class Solution {
+public:
+    bool isSafe(vector<string>& board,int r,int c,int n)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(board[r][j]=='Q')
+                return false;
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            if(board[i][c]=='Q')
+                return false;
+        }
+
+        for(int i=r, j=c; i>=0&&j>=0; i--,j--)
+        {
+            if(board[i][j]=='Q')
+                return false;
+        }
+
+        for(int i=r, j=c; i>=0&&j<n; i--,j++)
+        {
+            if(board[i][j]=='Q')
+                return false;
+        }
+        return true;
+
+    }
+
+    void nqueens(vector<string>& board,vector<vector<string>>& ans, int r, int n)
+    {
+        if(r==n)
+        {
+            ans.push_back({board});
+            return;
+        }
+
+        for(int j=0;j<n;j++)
+        {
+            if(isSafe(board,r,j,n))
+            {
+                board[r][j]='Q';
+                nqueens(board,ans,r+1,n);
+                board[r][j]='.';
+            }
+
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n,string(n,'.'));
+        vector<vector<string>> ans;
+
+        nqueens(board,ans,0,n);
+        return ans;
+    }
+};
