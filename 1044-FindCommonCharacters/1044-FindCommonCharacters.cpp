b560bef3f98fdb290bfae9f4b614659772511711
GyayakP <gyayakpatodi@gmail.com>
2026-07-06 18:49:15 +0530

    Create 1044-FindCommonCharacters

---
+++ 1044-FindCommonCharacters/1044-FindCommonCharacters.cpp
@@ -0,0 +1,30 @@
+// Last updated: 7/6/2026, 6:49:15 PM
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) 
+    {
+        vector<int> freq(26, INT_MAX);
+        for(string word : words)
+        {
+            vector<int> temp(26, 0);
+            for(char c : word)
+            {
+                temp[c - 'a']++;
+            }
+            for(int i = 0; i < 26; i++)
+            {
+                freq[i] = min(freq[i], temp[i]);
+            }
+        }
+        vector<string> ans;
+        for(int i = 0; i < 26; i++)
+        {
+            while(freq[i] > 0)
+            {
+                ans.push_back(string(1, i + 'a'));
+                freq[i]--;
+            }
+        }
+        return ans;
+    }
+};
