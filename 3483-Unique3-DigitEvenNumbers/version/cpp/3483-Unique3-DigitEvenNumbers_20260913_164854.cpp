a768d0c8b473aaf12c42084c7f6712c80138a02c
GyayakP <gyayakpatodi@gmail.com>
2026-09-13 16:48:55 +0530

    Create 3483-Unique3-DigitEvenNumbers

---
+++ 3483-Unique3-DigitEvenNumbers/version/cpp/3483-Unique3-DigitEvenNumbers_20260913_164854.cpp
@@ -0,0 +1,11 @@
+// Last updated: 9/13/2026, 4:48:54 PM
+1class Solution {
+2public:
+3    int countCommas(int n) {
+4        if(n>999)
+5        {
+6            return n-999;
+7        }
+8        return 0;
+9    }
+10};
